cdd5b7f581f8065844f9a1b97885dde824167dc6
Hasan Karahan <hasan.karahan@blackhan.com>
2015-11-08 14:28:47 +0400

    Re-organized project as RPC Client/Server w/Makefile(s)

--- server/py/server.py
+++ server/py/server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 ###############################################################################
 
+import argparse, os
 import tornado.web
 import tornado.websocket
 import tornado.ioloop
@@ -33,7 +34,20 @@ application = tornado.web.Application ([
 
 if __name__ == "__main__":
 
-    application.listen (8888)
+    parser = argparse.ArgumentParser (prog='RPC Server',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('-v', '--version', action='version',
+        version='%(prog)s 0.0.1')
+    parser.add_argument ('port', metavar='PORT', type=int,
+        default=os.environ.get ('RPC_PORT', 8088), nargs='?',
+        help='Server Port')
+    parser.add_argument ('host', metavar='HOST', type=str,
+        default=os.environ.get ('RPC_HOST', 'localhost'), nargs='?',
+        help='Server Host')
+
+    args = parser.parse_args ()
+    application.listen (args.port, args.host)
     tornado.ioloop.IOLoop.instance ().start ()
 
 ###############################################################################
