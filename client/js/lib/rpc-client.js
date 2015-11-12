162efd6a66fd8a5446b8352ea83563553fc3d398
Hasan Karahan <hasan.karahan@blackhan.com>
2015-11-12 15:53:11 +0400

    Client: variable number of workers

--- client/js/lib/rpc-client.js
+++ client/js/lib/rpc-client.js
@@ -45,71 +45,94 @@ var system_service = new Service(url, Space.System.Service, {
 
 system_service.socket.on('open', function () {
 
-    var intervalId0 = setInterval(function () {
-        var pair = new Space.System.Pair({
-            lhs: random(0, 256), rhs: random(0, 256)
-        });
+    var add_n = 1, add_interval_id = {};
+    var sub_n = 1, sub_interval_id = {};
+    var mul_n = 1, mul_interval_id = {};
+    var div_n = 1, div_interval_id = {};
 
-        var t0 = now();
-        system_service.api.add(pair, function (error, result) {
-            if (error !== null) throw error;
+    for (var add_i = 0; add_i < add_n; add_i++) {
+        add_interval_id[add_i] = setInterval(function () {
+            var pair = new Space.System.Pair({
+                lhs: random(0, 256), rhs: random(0, 256)
+            });
 
-            assert.equal(pair.lhs + pair.rhs, result.value);
-            console.log('T0:', now() - t0);
-        });
-    }, 0);
+            var t = process.hrtime();
+            system_service.api.add(pair, function (error, result) {
+                if (error !== null) throw error;
 
-    var intervalId1 = setInterval(function () {
-        var pair = new Space.System.Pair({
-            lhs: random(0, 256), rhs: random(0, 256)
-        });
+                assert.equal(pair.lhs + pair.rhs, result.value);
+                console.log('dT[add]:', process.hrtime(t));
+            });
+        }, 0);
+    }
 
-        var t1 = now();
-        system_service.api.sub(pair, function (error, result) {
-            if (error !== null) throw error;
+    for (var sub_i = 0; sub_i < sub_n; sub_i++) {
+        sub_interval_id[sub_i] = setInterval(function () {
+            var pair = new Space.System.Pair({
+                lhs: random(0, 256), rhs: random(0, 256)
+            });
 
-            assert.equal(pair.lhs - pair.rhs, result.value);
-            console.log('T1:', now() - t1);
-        });
-    }, 0);
+            var t = process.hrtime();
+            system_service.api.sub(pair, function (error, result) {
+                if (error !== null) throw error;
 
-    var intervalId2 = setInterval(function () {
-        var pair = new Space.System.Pair({
-            lhs: random(0, 256), rhs: random(0, 256)
-        });
+                assert.equal(pair.lhs - pair.rhs, result.value);
+                console.log('dT[sub]:', process.hrtime(t));
+            });
+        }, 0);
+    }
 
-        var t2 = now();
-        system_service.api.mul(pair, function (error, result) {
-            if (error !== null) throw error;
+    for (var mul_i = 0; mul_i < mul_n; mul_i++) {
+        mul_interval_id[mul_i] = setInterval(function () {
+            var pair = new Space.System.Pair({
+                lhs: random(0, 256), rhs: random(0, 256)
+            });
 
-            assert.equal(pair.lhs * pair.rhs, result.value);
-            console.log('T2:', now() - t2);
-        });
-    }, 0);
+            var t = process.hrtime();
+            system_service.api.mul(pair, function (error, result) {
+                if (error !== null) throw error;
 
-    var intervalId3 = setInterval(function () {
-        var pair = new Space.System.Pair({
-            lhs: random(0, 256), rhs: random(0, 256)
-        });
+                assert.equal(pair.lhs * pair.rhs, result.value);
+                console.log('dT[mul]:', process.hrtime(t));
+            });
+        }, 0);
+    }
 
-        var t3 = now();
-        system_service.api.div(pair, function (error, result) {
-            if (error !== null) throw error;
+    for (var div_i = 0; div_i < div_n; div_i++) {
+        div_interval_id[div_i] = setInterval(function () {
+            var pair = new Space.System.Pair({
+                lhs: random(0, 256), rhs: random(0, 256)
+            });
 
-            assert.ok(isNaN(pair.lhs / pair.rhs) === isNaN(result.value));
-            assert.ok(isFinite(pair.lhs/ pair.rhs) === isFinite(result.value));
-            assert.ok(isFinite(pair.lhs/ pair.rhs) === false ||
-                Math.abs(pair.lhs / pair.rhs - result.value) < 1E6);
+            var t = process.hrtime();
+            system_service.api.div(pair, function (error, result) {
+                if (error !== null) throw error;
+                var q = pair.lhs / pair.rhs;
 
-            console.log('T3:', now() - t3);
-        });
-    }, 0);
+                assert.ok(isNaN(q) === isNaN(result.value));
+                assert.ok(isFinite(q) === isFinite(result.value));
+                assert.ok(isFinite(q) === false ||
+                    Math.abs(q - result.value) < 1E6);
+
+                console.log('dT[div]:', process.hrtime(t));
+            });
+        }, 0);
+    }
 
     setTimeout(function () {
-        clearInterval(intervalId0);
-        clearInterval(intervalId1);
-        clearInterval(intervalId2);
-        clearInterval(intervalId3);
+        for (var add_key in add_interval_id)
+            if (add_interval_id.hasOwnProperty(add_key))
+                clearInterval(add_interval_id[add_key]);
+        for (var sub_key in sub_interval_id)
+            if (sub_interval_id.hasOwnProperty(sub_key))
+                clearInterval(sub_interval_id[sub_key]);
+        for (var mul_key in mul_interval_id)
+            if (mul_interval_id.hasOwnProperty(mul_key))
+                clearInterval(mul_interval_id[mul_key]);
+        for (var div_key in div_interval_id)
+            if (div_interval_id.hasOwnProperty(div_key))
+                clearInterval(div_interval_id[div_key]);
+
         system_service.socket.close();
     }, 10000);
 });
